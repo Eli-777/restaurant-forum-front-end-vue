5edb4f75d17c5b58d9be85b8fe75dce666cbe95c
Eli <jo45840@hotmail.com>
2020-07-09 21:38:12 +0800

    feat: add restaurant-comments component and add mixins.js

--- src/components/NewRestaurants.vue
+++ src/components/NewRestaurants.vue
@@ -26,17 +26,10 @@ small {
 </style>
 
 <script>
-import moment from 'moment'
+import { fromNowFilter} from './../utils/mixins'
 
 export default {
-  filters: {
-    fromNow (datetime) {
-      if (!datetime) {
-        return '-'
-      }
-      return moment(datetime).fromNow()
-    }
-  },
+  mixins: [fromNowFilter],
   props: {
     restaurants: {
       type: Array,
