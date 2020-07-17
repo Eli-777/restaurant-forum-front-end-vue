ea77eedd9ab8e25fa9500b21d84d94d7342fdcf7
Eli <jo45840@hotmail.com>
2020-07-17 15:58:05 +0800

    feat: add signup api

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -67,8 +67,9 @@
       <button
         class="btn btn-lg btn-primary btn-block mb-3"
         type="submit"
+        :disabled="isProcessing"
       >
-        Submit
+        {{ isProcessing ? '處理中...' : 'Submit'}}
       </button>
 
       <div class="text-center mb-3">
@@ -87,6 +88,9 @@
 </template>
 
 <script>
+import authorizationAPI from './../apis/authorization'
+import { Toast } from './../utils/helpers'
+
 export default {
   data () {
     return {
@@ -94,18 +98,58 @@ export default {
       email: '',
       password: '',
       passwordCheck: '',
+      isProcessing: false
     }
   },
   methods: {
-    handeleSubmit () {
-      const data = JSON.stringify({
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        passwordCheck: this.passwordCheck
-      })
-      // TODO: 向後端驗證使用者登入資訊是否合法
-      console.log('data', data)
+    async handeleSubmit () {
+      try {
+        this.isProcessing = true
+        if (!this.name || !this.email || !this.password || !this.passwordCheck) {
+          Toast.fire({
+            icon: 'error',
+            title: '請確認已填寫所有欄位'
+          })
+          this.isProcessing = false
+          return
+        } else if (this.password !== this.passwordCheck ) {
+          Toast.fire({
+            icon: 'error',
+            title: '兩次輸入密碼不同'
+          })
+          this.isProcessing = false
+          this.passwordCheck = ''
+          return
+        } 
+
+        const {data} = await authorizationAPI.signUp({ 
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          passwordCheck: this.passwordCheck
+         })
+
+        if (data.status !== 'success'){
+          throw new Error(data.message)
+        }
+
+        Toast.fire({
+          icon: 'success',
+          title: data.message
+        })
+
+        this.$router.push({ name: '/signin '})
+      
+       
+      } catch (error) {
+        this.isProcessing = false
+        console.log('error', error)
+        Toast.fire({
+          icon: 'error',
+          title: '無法新增帳號，請稍後再試'
+        })
+      }
+      
     }
   }
 }
